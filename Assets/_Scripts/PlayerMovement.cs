42e444e0841f4e429e680f19d84edfe81618756f
Isabel Lammens <isabel.lammens@gmail.com>
2020-09-22 22:27:01 +0200

    Switched to SteamVR

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed = 1f;
+
+    private CharacterController _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        _player.Move(speed * Time.deltaTime * direction - (new Vector3(0, -9.8f, 0) * Time.deltaTime));
+    }
+}
