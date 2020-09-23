5a881f8f7e7002c2400fdaf37431f2a55ea5feab
Isabel Lammens <isabel.lammens@gmail.com>
2020-09-23 18:09:45 +0200

    Added ambient sounds

---
+++ Assets/_Scripts/Repeat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repeat : MonoBehaviour
+{
+    private AudioSource _audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        StartCoroutine(repeat());
+    }
+
+    IEnumerator repeat()
+    {
+        _audio.Play();
+        yield return new WaitForSeconds(UnityEngine.Random.Range(50f, 70f));
+        StartCoroutine(repeat());
+
+        yield return null;
+    }
+}
