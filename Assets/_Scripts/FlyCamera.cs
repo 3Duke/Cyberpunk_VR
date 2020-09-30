2c230474fadb7385d8e29608628aecfea5e2e746
Isabel Lammens <isabel.lammens@gmail.com>
2020-09-30 21:46:53 +0200

    Added fire & steam vfx

---
+++ Assets/_Scripts/FlyCamera.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCamera : MonoBehaviour
+{
+
+    /*
+    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+    Converted to C# 27-02-13 - no credit wanted.
+    Simple flycam I made, since I couldn't find any others made public.  
+    Made simple to use (drag and drop, done) for regular keyboard layout  
+    wasd : basic movement
+    shift : Makes camera accelerate
+    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+
+
+    float mainSpeed = 2.0f; //regular speed
+    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 1000.0f; //Maximum speed when holdin gshift
+    float sensitivity = 0.5f; //How sensitive it with mouse
+    private float totalRun = 1.0f;
+
+    private float mouseX;
+    private float mouseY;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        //mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        //mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        //if (mouseX != 0)
+        //{
+        //    transform.rotation = Quaternion.Euler(mouseX, 0, 0);
+        //    //mouseX = Mathf.Clamp(mouseX, -90.0f, 90.0f);
+        //}
+        //if (mouseY != 0)
+        //{
+        //    transform.rotation = Quaternion.Euler(0, mouseY, 0);
+        //    //mouseY = Mathf.Clamp(mouseY, -90.0f, 90.0f);
+        //}
+
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler(new Vector3(-1f * (mouseY * 180f) * sensitivity, mouseX * 360f * sensitivity, transform.localRotation.z));
+
+        //Keyboard commands
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        { //If player wants to move on X and Z axis only
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.Translate(p);
+        }
+
+    }
+
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        return p_Velocity;
+    }
+}
